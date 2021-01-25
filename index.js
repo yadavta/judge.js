c8a483d84e697ad1cd7ca589d8ebd3445b693825
yadavta <tanushyadav@gmail.com>
2021-01-25 00:18:54 -0800

    mongodb changes

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
-const url = 'mongodb://localhost:27017';
-const dbName = 'judgejs';
 
-MongoClient.connect(url, function(err, client) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
- 
-  const db = client.db(dbName);
- 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://yadavta:J1BfJKsaB3gvP60b@judgejs.hfqca.mongodb.net/judgeJS?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("jugdeJS").collection("tournaments");
+  console.log("worked");
   client.close();
 });
+
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
