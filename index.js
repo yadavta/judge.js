c4686a5b96a9a1557f199b01da6cbd3ca6a7f239
yadavta <tanushyadav@gmail.com>
2021-01-25 00:28:25 -0800

    writing to mongo

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const PORT = process.env.PORT || 5000
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://yadavta:J1BfJKsaB3gvP60b@judgejs.hfqca.mongodb.net/judgeJS?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
+
+/*client.connect(err => {
   const collection = client.db("jugdeJS").collection("tournaments");
   const pizzaDocument = {
   name: "Neapolitan pizza",
@@ -15,7 +16,26 @@ client.connect(err => {
   const result = await pizzaCollection.insertOne(pizzaDocument);
   console.dir(result.insertedCount); // should print 1 on successful insert
   client.close();
-});
+});*/
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("judgeJS");
+    const collection = database.collection("tournaments");
+    // create a document to be inserted
+    const doc = { name: "Red", town: "kanto" };
+    const result = await collection.insertOne(doc);
+    console.log(
+      `${result.insertedCount} documents were inserted with the _id: ${result.insertedId}`,
+    );
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
