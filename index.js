ff80393e478d69b296a858f52b5b58e903ba3eb7
yadavta <tanushyadav@gmail.com>
2021-01-25 00:06:56 -0800

    trying mongodb

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const url = 'mongodb://localhost:27017';
+const dbName = 'judgejs';
+
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+ 
+  const db = client.db(dbName);
+ 
+  client.close();
+});
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
