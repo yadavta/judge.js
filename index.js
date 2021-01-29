06ad45870224e4b37efe3b0745abc9347d878332
yadavta <tanushyadav@gmail.com>
2021-01-28 23:26:36 -0800

    Update index.js

--- index.js
+++ index.js
@@ -6,30 +6,7 @@ const PORT = process.env.PORT || 5000
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://yadavta:J1BfJKsaB3gvP60b@judgejs.hfqca.mongodb.net/judgeJS?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
-var jsonParser = bodyParser.json()
-
-
-async function testing (data) {
-  try {
-  	var result;
-    await client.connect();
-    const database = client.db("judgejs");
-    const collection = database.collection("tournaments");
-    // create a document to be inserted
-    var d = new Date();
-	var year = d.getFullYear();
-   	const doc = {
-	    "_id" : (data.tournamentName.toLowerCase() + year),
-	    "name" : data.tournamentName,
-	    "tabroomName" : data.tabroomName,
-	    "schoolApproved" : data.schoolApproved
-    };
-    result = await collection.insertOne(doc);    
-  } finally {
-    await client.close();
-    return result;
-  }
-}
+var jsonParser = bodyParser.json();
 
 
 var app = express()
@@ -40,7 +17,34 @@ app.get('/', (req, res) => res.render('pages/index'))
 app.get('/about',(req,res)=> res.render('pages/about'))
 app.get('/create', (req,res)=> res.render('pages/create'))
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+async function createTournament (data) {
+	var result = "";
+	try {
+		await client.connect();
+		const database = client.db("judgejs");
+		const collection = database.collection("tournaments");
+		// create a document to be inserted
+		var d = new Date();
+		var year = d.getFullYear();
+	   	const doc = {
+			"_id" : (data.tournamentName.toLowerCase() + year),
+			"name" : data.tournamentName,
+			"tabroomName" : data.tabroomName,
+			"schoolApproved" : data.schoolApproved
+		};
+	
+		result = await collection.insertOne(doc);
+		
+	} finally {
+	    await client.close();
+ 	}
+ 	return result;
+}
+
+
 app.post('/about', jsonParser, function (req, res) {
-	var x = testing(req.body);
-	res.send(x)
+	let returnable = createTournament(req.body);
+	res.send(returnable);
 })
