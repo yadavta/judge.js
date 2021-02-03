7fce47e7a51ee06de9968b6e548427753576fac4
yadavta <tanushyadav@gmail.com>
2021-02-03 00:02:35 -0800

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 async function createTournament (data) {
   const uri = "mongodb+srv://yadavta:J1BfJKsaB3gvP60b@judgejs.hfqca.mongodb.net/judgejs?retryWrites=true&w=majority";
-  mongoose.connect('uri', {useNewUrlParser: true, useUnifiedTopology: true});
+  mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
   const db = mongoose.connection;
   db.on('error', console.error.bind(console, 'connection error:'));
   db.once('open', function() {
