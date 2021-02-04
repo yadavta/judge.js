6759fd3d65e5cff9f90dde73c1cbfa9168911ae0
yadavta <tanushyadav@gmail.com>
2021-02-03 23:27:04 -0800

    Update index.js

--- index.js
+++ index.js
@@ -29,51 +29,58 @@ async function listTournaments() {
 app.post('/tournament', function(req, res) {});
 
 app.post('/createTournament', jsonParser, function(req, res) {
-    res.send("yo")
-    /*let data = req.body;
-    const uri = "mongodb+srv://yadavta:J1BfJKsaB3gvP60b@judgejs.hfqca.mongodb.net/judgejs?retryWrites=true";
-    mongoose.connect(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
-    const db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error:'));
-    db.once('open', async function() {
-        // all mongo code goes here
-        const tournamentSchema = new mongoose.Schema({
-            _id: String,
-            tournamentName: String,
-            tabroomName: String,
-            circuits: Array,
-            judgeTypes: Array,
-            schoolApproved: Boolean,
-            asIndependent: Boolean,
-            startDate: Date,
-            endDate: Date,
+    //res.send("yo");
+    let data = req.body;
+
+    async function() {
+        const uri = "mongodb+srv://yadavta:J1BfJKsaB3gvP60b@judgejs.hfqca.mongodb.net/judgejs?retryWrites=true";
+        mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
         });
+        try{
+            const db = mongoose.connection;
+            db.on('error', console.error.bind(console, 'connection error:'));
+            db.once('open', async function() {
+                // all mongo code goes here
+                const tournamentSchema = new mongoose.Schema({
+                    _id: String,
+                    tournamentName: String,
+                    tabroomName: String,
+                    circuits: Array,
+                    judgeTypes: Array,
+                    schoolApproved: Boolean,
+                    asIndependent: Boolean,
+                    startDate: Date,
+                    endDate: Date,
+                });
 
-        tournamentSchema.methods.speak = function() {
-            console.log(this.tournamentName);
-        }
+                tournamentSchema.methods.speak = function() {
+                    console.log(this.tournamentName);
+                }
 
-        const Tournament = mongoose.model('Tournament', tournamentSchema);
+                const Tournament = mongoose.model('Tournament', tournamentSchema);
 
-        const tourneyAtHand = new Tournament({
-            _id: data._id,
-            tournamentName: data.tournamentName,
-            tabroomName: data.tabroomName,
-            circuits: data.circuits,
-            judgeTypes: data.judgeTypes,
-            schoolApproved: data.schoolApproved,
-            asIndependent: data.asIndependent,
-            startDate: data.startDate,
-            endDate: data.endDate
-        });
+                const tourneyAtHand = new Tournament({
+                    _id: data._id,
+                    tournamentName: data.tournamentName,
+                    tabroomName: data.tabroomName,
+                    circuits: data.circuits,
+                    judgeTypes: data.judgeTypes,
+                    schoolApproved: data.schoolApproved,
+                    asIndependent: data.asIndependent,
+                    startDate: data.startDate,
+                    endDate: data.endDate
+                });
 
-        await tourneyAtHand.save()
-            .then(function(value) {
-                res.send(value);
+                await tourneyAtHand.save()
+                    .then(function(value) {
+                        res.send(value);
+                    });
+
             });
-
-    });*/
+        }  catch {
+            res.send("error");
+        }
+    }
 });
