c8297a4a16850d92b3313fcb0c58f41d36111c0e
yadavta <tanushyadav@gmail.com>
2021-02-03 23:23:13 -0800

    Update index.js

--- index.js
+++ index.js
@@ -21,50 +21,6 @@ app.get('/tournament', (req, res) => res.render('pages/tournament'))
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
-async function createTournament(data) {
-    const uri = "mongodb+srv://yadavta:J1BfJKsaB3gvP60b@judgejs.hfqca.mongodb.net/judgejs?retryWrites=true&w=majority";
-    mongoose.connect(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
-    const db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error:'));
-    db.once('open', function() {
-        // all mongo code goes here
-        const tournamentSchema = new mongoose.Schema({
-            _id: String,
-            tournamentName: String,
-            tabroomName: String,
-            circuits: Array,
-            judgeTypes: Array,
-            schoolApproved: Boolean,
-            asIndependent: Boolean,
-            startDate: Date,
-            endDate: Date,
-        });
-
-        tournamentSchema.methods.speak = function() {
-            console.log(this.tournamentName);
-        }
-
-        const Tournament = mongoose.model('Tournament', tournamentSchema);
-
-
-        const tourneyAtHand = new Tournament({
-            _id: data._id,
-            tournamentName: data.tournamentName,
-            tabroomName: data.tabroomName,
-            circuits: data.circuits,
-            judgeTypes: data.judgeTypes,
-            schoolApproved: data.schoolApproved,
-            asIndependent: data.asIndependent,
-            startDate: data.startDate,
-            endDate: data.endDate
-        });
-        tourneyAtHand.save();
-    });
-
-}
 
 async function listTournaments() {
 
@@ -73,7 +29,8 @@ async function listTournaments() {
 app.post('/tournament', function(req, res) {});
 
 app.post('/createTournament', jsonParser, function(req, res) {
-    let data = req.body;
+    res.send("yo")
+    /*let data = req.body;
     const uri = "mongodb+srv://yadavta:J1BfJKsaB3gvP60b@judgejs.hfqca.mongodb.net/judgejs?retryWrites=true";
     mongoose.connect(uri, {
         useNewUrlParser: true,
@@ -118,5 +75,5 @@ app.post('/createTournament', jsonParser, function(req, res) {
                 res.send(value);
             });
 
-    });
+    });*/
 });
