10094a43f59c504f8ed620cb1e0709c5fbd5027f
yadavta <tanushyadav@gmail.com>
2021-02-03 22:37:32 -0800

    mongoose attempt
    first tried in repl.it

--- index.js
+++ index.js
@@ -71,6 +71,47 @@ app.post('/tournament', function (req, res) {
 });
 
 app.post('/createTournament', jsonParser, function (req, res) {
-	//createTournament(req.body);
-  res.send("hello world");
+    let data = req.body;
+    const uri = "mongodb+srv://yadavta:J1BfJKsaB3gvP60b@judgejs.hfqca.mongodb.net/judgejs?retryWrites=true&w=majority";
+    mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', async function() {
+    	// all mongo code goes here
+    	const tournamentSchema = new mongoose.Schema({
+    	    _id: String,
+    		tournamentName: String,
+    		tabroomName: String,
+    		circuits: Array,
+    		judgeTypes: Array,
+    		schoolApproved: Boolean,
+    		asIndependent: Boolean,
+    		startDate: Date,
+    		endDate: Date,
+    	});
+
+    	tournamentSchema.methods.speak = function () {
+    		console.log(this.tournamentName);
+    	}
+
+    	const Tournament = mongoose.model('Tournament', tournamentSchema);
+
+		const tourneyAtHand = new Tournament({
+    		_id : data._id,
+    		tournamentName : data.tournamentName,
+    		tabroomName : data.tabroomName,
+    		circuits : data.circuits,
+    		judgeTypes : data.judgeTypes,
+    		schoolApproved : data.schoolApproved,
+    		asIndependent : data.asIndependent,
+    		startDate : data.startDate,
+    		endDate : data.endDate
+		});
+
+		await tourneyAtHand.save()
+    		.then(function(value) {
+        		res.send(value);
+		});
+
+		});
 });
