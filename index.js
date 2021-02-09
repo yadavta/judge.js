138d28ff65bba51b8eef14a45b122b0530659ce3
yadavta <tanushyadav@gmail.com>
2021-02-08 19:04:26 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,84 +1,82 @@
 const express = require('express')
 var bodyParser = require('body-parser')
 const path = require('path')
-const MongoClient = require('mongodb').MongoClient;
 const mongoose = require('mongoose');
+const MongoClient = require('mongodb').MongoClient;
 const PORT = process.env.PORT || 5000
+
+const uri = "mongodb+srv://yadavta:J1BfJKsaB3gvP60b@judgejs.hfqca.mongodb.net/judgejs?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
 var jsonParser = bodyParser.json();
 
-//MONGOOSE
-
-//set up connection
 
 var app = express()
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('pages/index'))
-app.get('/about', (req, res) => res.render('pages/about'))
-app.get('/create', (req, res) => res.render('pages/create'))
-app.get('/tournament', (req, res) => res.render('pages/tournament'))
+app.get('/about',(req,res)=> res.render('pages/about'))
+app.get('/create', (req,res)=> res.render('pages/create'))
+app.get('/tournament', (req,res)=> res.render('pages/tournament'))
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
+async function createTournament (hack) {
+	/*var result = "";
+	try {
+		await client.connect();
+		const collection = client.db("judgejs").collection("tournaments");
+		// create a document to be inserted
+			const doc = {
+				"_id" : data._id,
+				"tournamentName" : data.tournamentName,
+				"tabroomName" : data.tabroomName,
+				"circuits" : data.circuits,
+				"judgeTypes" : data.judgeTypes,
+				"schoolApproved" : data.schoolApproved,
+				"asIndependent" : data.asIndependent,
+				"startDate" : data.startDate,
+				"endDate" : data.endDate
+			}
 
-async function listTournaments() {
+		await collection.insertOne(doc)
+		.then(function(response) {
+			result = response;
+		});
 
+	} finally {
+	    await client.close();
+ 	}
+	return result;*/
 }
 
-app.post('/tournament', function(req, res) {});
+async function listTournaments() {
+	var result = "";
+	try {
+		await client.connect();
+		const collection = client.db("judgejs").collection("tournaments");
+		collection.find().toArray(function(err,response) {
+			result = response;
+		});
+	}
+	catch (e) {
+		result = e;
+	}
+	finally {
+	    await client.close();
+ 	}
+	return result;
+}
 
-app.post('/createTournament', jsonParser, function(req, res) {
-    //res.send("yo");
-    let data = req.body;
+app.post('/tournament', function (req, res) {
+	listTournaments()
+	.then(function(result) {
+		res.send(result);
+	});
+});
 
-        const uri = "mongodb+srv://yadavta:J1BfJKsaB3gvP60b@judgejs.mongodb.net/judgejs?retryWrites=true&w=majority";
-        mongoose.connect(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        try{
-            const db = mongoose.connection;
-            db.on('error', console.error.bind(console, 'connection error:'));
-            db.once('open', async function() {
-                // all mongo code goes here
-                const tournamentSchema = new mongoose.Schema({
-                    _id: String,
-                    tournamentName: String,
-                    tabroomName: String,
-                    circuits: Array,
-                    judgeTypes: Array,
-                    schoolApproved: Boolean,
-                    asIndependent: Boolean,
-                    startDate: Date,
-                    endDate: Date,
-                });
-
-                tournamentSchema.methods.speak = function() {
-                    console.log(this.tournamentName);
-                }
-
-                const Tournament = mongoose.model('Tournament', tournamentSchema);
-
-                const tourneyAtHand = new Tournament({
-                    _id: data._id,
-                    tournamentName: data.tournamentName,
-                    tabroomName: data.tabroomName,
-                    circuits: data.circuits,
-                    judgeTypes: data.judgeTypes,
-                    schoolApproved: data.schoolApproved,
-                    asIndependent: data.asIndependent,
-                    startDate: data.startDate,
-                    endDate: data.endDate
-                });
-
-                await tourneyAtHand.save()
-                    .then(function(value) {
-                        res.send(value);
-                    });
-
-            });
-        } catch (e) {
-            res.send("error");
-        }
+app.post('/create', jsonParser, function (req, res) {
+	let returnable = createTournament(req.body);
+	res.send(returnable);
 });
