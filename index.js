a05c9c770817fa16f6a1c13e3a0aeae808854da2
yadavta <tanushyadav@gmail.com>
2021-02-22 21:32:39 -0800

    learning to spell
    process not proccess

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ var AWS = require("aws-sdk");
 AWS.config.update({
   region: "us-west-2",
   endpoint: "https://dynamodb.us-west-2.amazonaws.com",
-  accessKeyId: proccess.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: proccess.env.AWS_SECRET_ACCESS_KEY
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
 
 var docClient = new AWS.DynamoDB.DocumentClient();
